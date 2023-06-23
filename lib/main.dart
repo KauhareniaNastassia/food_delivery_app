7b8632fa3e840201fb63479cae39e2ae6ebb21dd
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-06-23 15:18:16 +0300

    [PT-1] Project setup Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:core/config/di/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
